917e0f2b1a413cd8db9d7093dd580d1294a3763e
myathtut-zafir <aa.htut@gmail.com>
2024-11-03 13:26:50 +0700

    feat: overall setup before call llm

--- main.py
+++ main.py
@@ -3,14 +3,43 @@ import streamlit as st
 import pickle
 import time
 import langchain
+import copy
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from langchain.chains.qa_with_sources.retrieval import RetrievalQAWithSourcesChain
 from langchain.chains.qa_with_sources.loading import load_qa_with_sources_chain
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.document_loaders import UnstructuredURLLoader
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
+from langchain_community.document_loaders import UnstructuredURLLoader
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
 
 load_dotenv()
 
+# llm = ChatOpenAI(temperature=0, model_name="gpt-4o-mini", max_tokens=500)
+loaders = UnstructuredURLLoader(
+    urls=[
+        "https://www.moneycontrol.com/news/business/markets/wall-street-rises-as-tesla-soars-on-ai-optimism-11351111.html",
+        "https://www.moneycontrol.com/news/business/tata-motors-launches-punch-icng-price-starts-at-rs-7-1-lakh-11098751.html",
+    ]
+)
+data= loaders.load()
+
+text_splitter=RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=200,
+    )
+docs=text_splitter.split_documents(data)
+
+embeddings=OpenAIEmbeddings()
+vectorDB=FAISS.from_documents(docs,embeddings)
+
+# simple query
+# query = "how much Walt Disney (DIS.N) added?"
+# docs = vectorDB.similarity_search(query)
+# print(docs[0].page_content)
+    
+
+# print(docs[0])
+# res = llm.invoke("hello")
+
+# print(res)
