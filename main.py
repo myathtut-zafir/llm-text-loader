1b35db611a05818437014662fb31048a7e4bc8b7
myathtut-zafir <aa.htut@gmail.com>
2024-11-03 13:56:34 +0700

    feat: add ollama and llama

--- main.py
+++ main.py
@@ -12,10 +12,12 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import UnstructuredURLLoader
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
+from langchain_ollama import ChatOllama
 
 load_dotenv()
 
-llm = ChatOpenAI(temperature=0, model_name="gpt-4o-mini", max_tokens=500)
+# llm = ChatOpenAI(temperature=0, model_name="gpt-4o-mini", max_tokens=500)
+llm = ChatOllama(temperature=0, model="llama3.2", max_tokens=500)
 loaders = UnstructuredURLLoader(
     urls=[
         "https://www.moneycontrol.com/news/business/markets/wall-street-rises-as-tesla-soars-on-ai-optimism-11351111.html",
@@ -33,18 +35,21 @@ docs=text_splitter.split_documents(data)
 embeddings=OpenAIEmbeddings()
 vectorDB=FAISS.from_documents(docs,embeddings)
 
-# simple query
+# simple query - note
 # query = "how much Walt Disney (DIS.N) added?"
 # docs = vectorDB.similarity_search(query)
 # print(docs[0].page_content)
     
 chain=RetrievalQAWithSourcesChain.from_llm(llm=llm,retriever=vectorDB.as_retriever())
 query = "what is the price of Tiago iCNG?"
-# query = "what are the main features of punch iCNG?"
 
 langchain.debug=True
 
 print(chain({"question": query}, return_only_outputs=True))
+
+
+
+#note
 # print(docs[0])
 # res = llm.invoke("hello")
 
